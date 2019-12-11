81641b80f24b11c5f00ced57df585eda4018c20b
zhoujialin <1599200510@qq.com>
2019-12-11 17:39:15 +0800

    rc

---
+++ home_work/day2_homework/day2_os/day2os.py
@@ -0,0 +1,10 @@
+# 任务1-将该目录下的文件按照后缀进行分类，然后分别新建且放入不同的文件夹内，比如txt文件放入txt目录下等
+import os
+print(os.path)
+os.path.split(os.path)
+
+
+# for root, dirs, files in os.walk(file_dir):
+#     print('root_dir:', root)  # 当前目录路径
+#     print('sub_dirs:', dirs)  # 当前路径下所有子目录
+#     print('files:', files)  # 当前路径下所有非目录子文件
--- home_work/day2_homework/day2_str.py
+++ home_work/day2_homework/day2_str.py
@@ -4,22 +4,43 @@
 任务3-找出所有女性用户的信息
 """
 # 任务1-找出所有L开头的人名
+re_1 = []
+re_3 = []
+mp = []
+delete_space = []
+# 读取文件
+with open("personal_information", "r",encoding="utf-8") as f:
+    lines = f.readlines()
+    # print(lines)
+    # 去除换行符
+    for line in lines:
+        line = line.rsplit()
+        delete_space.append(line)
+    # print(delete_space)
 
-# delete_space = []
-# with open("personal_information", "r",encoding="utf-8") as f:
-#     lines = f.readlines()
-#     print(lines)
-#     # 去除换行符
-#     for line in lines:
-#         line = line.rsplit()
-#         delete_space.append(line)
-#     print(delete_space)
-#
-# for d_list in delete_space:
-#     for d_l in d_list:
-#         d_l.split("|")
-#     print()
 
-a = ['Lucy|18601914231|男|19890218']
-for i in a:
-    i.
+for d_list in delete_space:
+    for d_l in d_list:
+        # item = ['Lucy', '18601914231', '男', '19890218']
+        items = d_l.split("|")
+
+# 任务1
+        if items[0].startswith("L") == True:
+            re_1.append(items[0])
+# 任务3
+        if items[2] == "女":
+            re_3.append(items)
+
+        mp.append(items)
+
+# 使用冒泡排序完成成任务2
+n = len(mp)
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        if mp[j][3] > mp[j+1][3]:
+            mp[j] , mp[j+1] = mp[j+1], mp[j]
+
+print(re_1)
+print(re_3)
+print(mp)
