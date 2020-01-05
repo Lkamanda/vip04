c029bf93be1ebb6638e7e698950908aeb9ef4f79
zhoujialin <18612463553@163.com>
2020-01-06 00:29:16 +0800

    interfacems

--- home_work/day4_homework/连接redis.py
+++ home_work/day4_homework/连接redis.py
@@ -8,6 +8,6 @@ r.set('book', '西游记')
 print(r.get('book'))
 
 # 常规连接
-r = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
+r = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True, db='')
 r.set('name', 'shuke')
 print(r.get('name'))
---
+++ home_work/day5_homework/jk/readExcal.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2020-01-05 23:24
+@Author  : zhoujialin
+@File    : readExcal.py
+'''
+import unittest
+import xlrd
+import xlutils
+import configparser
+import HTMLTestRunner
+import logging
